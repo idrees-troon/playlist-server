a2ce34f60ac40034072cbf334daacfa45dd5898c
idrees-troon <33491348+Idrees66@users.noreply.github.com>
2024-12-22 22:48:42 +0500

    update db name

--- database.js
+++ database.js
@@ -1,20 +1,14 @@
-// File: database.js
-
 const mongoose = require('mongoose');
 
 // MongoDB connection string
-const MONGO_URI = 'mongodb://localhost:27017/myDatabase'; // Replace with your DB URI
+const MONGO_URI = 'mongodb://localhost:27017/playlist'; // Replace with your DB URI
 
 // Function to connect to the database
 async function connectToDatabase() {
+    console.log("trying to connect to database");
     try {
         // Connect to MongoDB
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true, // Parse MongoDB connection string
-            useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
-            useCreateIndex: true, // Ensure indexes are created automatically
-            useFindAndModify: false, // Use native `findOneAndUpdate()` instead of deprecated methods
-        });
+        await mongoose.connect(MONGO_URI);
         console.log('Successfully connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
@@ -22,5 +16,4 @@ async function connectToDatabase() {
     }
 }
 
-// Export the connection function
 module.exports = connectToDatabase;
